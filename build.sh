9d02228aef3fe16ef50c73e8e95bb6dab8fe27bd
PhanSonLoc <phansonloc1999@gmail.com>
2021-06-22 16:59:43 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 boon build . --target windows --version 11.0
+git tag -d latest
+git tag latest
+git push origin latest --force
 gh release delete latest --yes
-gh release create latest ./release/Historically-Accurate-Jam-win64.zip ./release/Historically-Accurate-Jam.love
+gh release create latest --notes "" --prerelease ./release/Historically-Accurate-Jam-win64.zip ./release/Historically-Accurate-Jam.love
