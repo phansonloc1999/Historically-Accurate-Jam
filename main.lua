47a992ee420de86899f15006511515e7419cbe55
PhanSonLoc <phansonloc1999@gmail.com>
2021-06-23 22:02:37 +0700

    Add more enemies

--- main.lua
+++ main.lua
@@ -96,6 +96,22 @@ function love.load()
                             sprite = Sprites.enemies.kiem
                         },
                         gridIndex = 4
+                    },
+                    {
+                        hero = {
+                            stats = {str = 5, dur = 5, int = 5, agi = 5},
+                            skill = "strike",
+                            sprite = Sprites.enemies.kiem
+                        },
+                        gridIndex = 1
+                    },
+                    {
+                        hero= {
+                            stats = {str = 5, dur = 5, int = 5, agi = 5},
+                            skill = "strike",
+                            sprite = Sprites.enemies.kiem,
+                    },
+                        gridIndex = 3
                     }
                 }
             },
